637ca85a7c64819edc9421d3686f125537189dd1
Bobleoble <felix@felixworkman.com>
2020-02-23 15:06:43 -0500

    Added secure-ssh script (WIP)

---
+++ SYS-265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author Bobleoble
+#creates a new ssh user using the $1 parameter
+#adds a public key from the local repo or curled from the remote repo
+#removes roots ability to ssh in
+echo "ALL YOUR CODE GOES HERE"

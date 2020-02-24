1a7f1229e4f42cf4b20a96d937cd9dedf75e2f5d
Bobleoble <felix@felixworkman.com>
2020-02-24 15:08:26 +0000

    Updated secure-ssh script with finalized version

--- SYS-265/linux/centos7/secure-ssh.sh
+++ SYS-265/linux/centos7/secure-ssh.sh
@@ -5,11 +5,10 @@
 #removes roots ability to ssh in
 
 sudo useradd -m -d /home/$1 -s /bin/bash $1
-sudo mkdir /home/$1/.ssh
-cd /home/$1/.ssh
-wget -O authorized_keys https://raw.githubusercontent.com/Bobleoble/tech-journal/master/SYS-265/linux/public-keys/id_rsa.pub
+sudo mkdir -p /home/$1/.ssh
+sudo wget -O /home/$1/.ssh/authorized_keys https://raw.githubusercontent.com/Bobleoble/tech-journal/master/SYS-265/linux/public-keys/id_rsa.pub
 
 sudo chmod 700 /home/$1/.ssh
 sudo chmod 600 /home/$1/.ssh/authorized_keys
-sudo chown -R sys265:sys265 /home/sys265/.ssh
-sed -i 's/PermitRootLogon yes/PermitRootLogon no/g' /etc/ssh/sshd_config
+sudo chown -R $1:$1 /home/$1/.ssh
+sudo sed -i 's/PermitRootLogon yes/PermitRootLogon no/g' /etc/ssh/sshd_config
